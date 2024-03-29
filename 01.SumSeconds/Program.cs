c43d5e4e185282116cfc2cc49bba270f98429e58
braqn1999 <117543750+braqn1999@users.noreply.github.com>
2024-03-29 00:06:37 -0500

    Add files via upload

---
+++ 01.SumSeconds/Program.cs
@@ -0,0 +1,17 @@
+﻿int firstTime = int.Parse(Console.ReadLine());
+int secondTime = int.Parse(Console.ReadLine());
+int thirdTime = int.Parse(Console.ReadLine());
+
+int totalTime = firstTime + secondTime + thirdTime;
+
+int minutes = totalTime / 60;
+int second = totalTime % 60;
+
+if (second < 10)
+{
+    Console.WriteLine($"{minutes}:0{second}");
+}
+else
+{
+    Console.WriteLine($"{minutes}:{second}");
+}
